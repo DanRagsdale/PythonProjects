f07602e87f37f0e8e0c0731dfe1d83f9d464bd86
Daniel Ragsdale <DanJeffRags@gmail.com>
2022-07-31 13:05:52 -0700

    Added image textures for the pieces

--- connect_four.py
+++ connect_four.py
@@ -1,5 +1,5 @@
-from turtle import back
 import pygame
+import sys, os
 
 from pygame.locals import (
 	K_1,
@@ -16,21 +16,39 @@ from pygame.locals import (
 	QUIT
 	)
 
+IMAGE_SOURCES = [
+	"piece_red.png",
+	"piece_yellow.png"
+]
+
+IMAGES = []
+
 pygame.init()
 
 pygame.display.set_caption("Connect 4")
 window = pygame.display.set_mode((800,600))
 
+module = sys.modules['__main__']
+path, name = os.path.split(module.__file__)
+
+for tag in IMAGE_SOURCES:
+	print(os.path.join(path, "res", "connect", tag))
+	img = pygame.image.load(os.path.join(path, "res", "connect", tag))
+	img.convert()
+
+	IMAGES.append(img)
+
+
 background = pygame.Surface((800,600))
 background.fill((0,0,0))
 
 
-board = [[2 for y in range(6)] for x in range(7)] 
+board = [[-1 for y in range(6)] for x in range(7)] 
 
 
 def drop_piece(column, type):
 	for i in range(5,-1,-1):
-		if board[column][i] == 2:
+		if board[column][i] == -1:
 			board[column][i] = type
 			return True
 	return False
@@ -48,20 +66,14 @@ while running:
 				current_turn = (current_turn + 1) % 2
 
 
-
-	print(K_2)
-
-
 	# Render Code
 	for x in range(0,7):
 		for y in range(0,6):
 			coord = pygame.Rect(x * 100, y * 100, 100, 100)
-			if board[x][y] == 0:
-				background.fill((255,255,0),coord)
-			elif board[x][y] == 1:
-				background.fill((255,0,0),coord)
-			elif board[x][y] == 2:
+			if board[x][y] == -1:
 				background.fill((128,128,128),coord)
+			else:
+				pygame.Surface.blit(background,IMAGES[board[x][y]], coord)
 
 	window.blit(background, (0,0))
 
