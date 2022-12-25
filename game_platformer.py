78e83974619ac44a58719aafd2b0d72dc016020f
Daniel Ragsdale <DanJeffRags@gmail.com>
2022-12-24 21:38:32 -0700

    Generalized entity system and added simple collectable coins.

--- game_platformer.py
+++ game_platformer.py
@@ -36,6 +36,17 @@ path, name = os.path.split(module.__file__)
 
 tex_dirt = pygame.image.load(os.path.join(path, "res", "platformer", "textures", "tex_dirt.png"))
 
+tex_coin = [
+	pygame.image.load(os.path.join(path, "res", "platformer", "sprites", "coin-00.png")).convert_alpha(),
+	pygame.image.load(os.path.join(path, "res", "platformer", "sprites", "coin-01.png")).convert_alpha(),
+	pygame.image.load(os.path.join(path, "res", "platformer", "sprites", "coin-02.png")).convert_alpha(),
+	pygame.image.load(os.path.join(path, "res", "platformer", "sprites", "coin-03.png")).convert_alpha(),
+	pygame.image.load(os.path.join(path, "res", "platformer", "sprites", "coin-04.png")).convert_alpha(),
+	pygame.image.load(os.path.join(path, "res", "platformer", "sprites", "coin-05.png")).convert_alpha(),
+	pygame.image.load(os.path.join(path, "res", "platformer", "sprites", "coin-06.png")).convert_alpha(),
+	pygame.image.load(os.path.join(path, "res", "platformer", "sprites", "coin-07.png")).convert_alpha(),
+]
+
 tex_idle = [
 	pygame.image.load(os.path.join(path, "res", "platformer", "sprites", "adventurer-idle-00.png")).convert_alpha(),
 	pygame.image.load(os.path.join(path, "res", "platformer", "sprites", "adventurer-idle-01.png")).convert_alpha(),
@@ -52,6 +63,7 @@ tex_run = [
 ]
 
 
+
 class Platform(pygame.sprite.Sprite):
 	def __init__(self, x, y):
 		super().__init__()
@@ -60,6 +72,32 @@ class Platform(pygame.sprite.Sprite):
 		self.rect = self.surf.get_rect(topleft=(x,y))
 
 		self.texture = tex_dirt 
+
+class Coin(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		super().__init__()
+		
+		self.surf = pygame.Surface((BLOCK_SIZE, BLOCK_SIZE))
+		self.surf.fill((255,255,0))
+		self.rect = self.surf.get_rect(topleft=(x,y))
+
+		self.texture = tex_coin[0]
+		self.anim_index = 0
+
+	def update(self, game):
+		collisions = pygame.sprite.spritecollide(self, game.entities, False)
+		for col in collisions:
+			if isinstance(col, Player):
+				self.kill()
+
+	def update_anims(self, game):
+		self.anim_index += 1
+		texture_list = tex_coin
+
+		if self.anim_index >= len(texture_list):
+			self.anim_index = 0
+		self.texture = texture_list[self.anim_index]
+
 
 class Player(pygame.sprite.Sprite):
 	class State:
@@ -122,7 +160,35 @@ class Player(pygame.sprite.Sprite):
 		self.pos += self.vel
 		self.rect.midbottom = self.pos
 
-	def update_anims(self):
+	def jump(self):
+		if self.on_ground > 0:
+			self.vel.y = JUMP_VEL
+
+	def update(self, game):
+		self.set_on_ground()
+
+		#ground_hits = pygame.sprite.spritecollide(self, platforms, False)
+		#for hit in ground_hits:
+		#	delta = (self.rect.center[0] - hit.rect.center[0], self.rect.center[1] - hit.rect.center[1])
+		#	if abs(delta[1]) - 1.5*abs(delta[0]) > 2:
+		#		if delta [1] < 0 and self.vel.y > 0:
+		#			self.pos.y = hit.rect.top + 1
+		#			self.vel.y = 0
+		#		elif delta[1] > 0:
+		#			self.pos.y = hit.rect.bottom + 2*BLOCK_SIZE
+		#			self.vel.y = 0
+
+		#wall_hits = pygame.sprite.spritecollide(self, platforms, False)
+		#for hit in wall_hits:
+		#	delta = (self.rect.center[0] - hit.rect.center[0], self.rect.center[1] - hit.rect.center[1])
+		#	if 1.5*abs(delta[0]) - abs(delta[1]) > 3:
+		#		if delta[0] > 0:
+		#			self.pos.x = hit.rect.right + BLOCK_SIZE / 2 - 1
+		#		else:
+		#			self.pos.x = hit.rect.left - BLOCK_SIZE / 2 + 1
+		#		self.vel.x = 0
+
+	def update_anims(self, game):
 		self.anim_index += 1
 		texture_list = Player.State.Textures[self.current_state]
 
@@ -132,7 +198,6 @@ class Player(pygame.sprite.Sprite):
 			self.texture = texture_list[self.anim_index]
 		else:
 			self.texture = pygame.transform.flip(texture_list[self.anim_index], True, False)
-
 
 	def update_distances(self, game):
 		#Raycast upward
@@ -195,33 +260,6 @@ class Player(pygame.sprite.Sprite):
 		self.on_ground -= 1
 			
 
-	def jump(self):
-		if self.on_ground > 0:
-			self.vel.y = JUMP_VEL
-
-	def update(self):
-		#ground_hits = pygame.sprite.spritecollide(self, platforms, False)
-		#for hit in ground_hits:
-		#	delta = (self.rect.center[0] - hit.rect.center[0], self.rect.center[1] - hit.rect.center[1])
-		#	if abs(delta[1]) - 1.5*abs(delta[0]) > 2:
-		#		if delta [1] < 0 and self.vel.y > 0:
-		#			self.pos.y = hit.rect.top + 1
-		#			self.vel.y = 0
-		#		elif delta[1] > 0:
-		#			self.pos.y = hit.rect.bottom + 2*BLOCK_SIZE
-		#			self.vel.y = 0
-
-		#wall_hits = pygame.sprite.spritecollide(self, platforms, False)
-		#for hit in wall_hits:
-		#	delta = (self.rect.center[0] - hit.rect.center[0], self.rect.center[1] - hit.rect.center[1])
-		#	if 1.5*abs(delta[0]) - abs(delta[1]) > 3:
-		#		if delta[0] > 0:
-		#			self.pos.x = hit.rect.right + BLOCK_SIZE / 2 - 1
-		#		else:
-		#			self.pos.x = hit.rect.left - BLOCK_SIZE / 2 + 1
-		#		self.vel.x = 0
-		pass
-
 class Game():
 	def __init__(self):
 		self.load_level("level_001.png")
@@ -233,6 +271,7 @@ class Game():
 		START_POS = vec(0,0)
 
 		self.platforms = pygame.sprite.Group()
+		self.entities = pygame.sprite.Group()
 
 		img = Image.open(os.path.join(path, "res", "platformer", "maps", level_name))
 		for x in range(img.size[0]):
@@ -242,18 +281,22 @@ class Game():
 				if pixel != (0,0,0):
 					if pixel == (0xff,0,0):
 						START_POS = vec(x * BLOCK_SIZE, y * BLOCK_SIZE + BLOCK_SIZE -2)
+					elif pixel == (0xff,0xff,0):
+						coin = Coin(x * BLOCK_SIZE, y * BLOCK_SIZE)
+						self.entities.add(coin)
 					else:
 						plat = Platform(x * BLOCK_SIZE, y * BLOCK_SIZE)
 						self.platforms.add(plat)
 
 		self.player = Player(START_POS)
+		self.entities.add(self.player)
 
 		self.game_map = pygame.Surface((img.size[0] * BLOCK_SIZE, img.size[1] * BLOCK_SIZE))
 		self.background = pygame.Surface((img.size[0] * BLOCK_SIZE, img.size[1] * BLOCK_SIZE))
 		self.background.fill((100,100,220))
 
-		for entity in self.platforms:
-			self.background.blit(tex_dirt, entity.rect)
+		for plat in self.platforms:
+			self.background.blit(tex_dirt, plat.rect)
 
 		self.window_pos = [0,0]
 
@@ -265,10 +308,13 @@ class Game():
 		state = 1
 		frame_count = 0
 		running = True
+
 		while running:
 			current_time = time.perf_counter()
 
-			#events = pygame.event.get()
+			if (current_time - last_frame) > 1.0 / 60:
+				print("Stuttering")
+
 
 			# Game Code
 			if state == 0:
@@ -285,13 +331,15 @@ class Game():
 								self.player.jump()
 
 					self.player.move(self)
-					self.player.update()
-					self.player.set_on_ground()
+
+					for entity in self.entities:
+						entity.update(self)
 
 					last_physics += 1.0 / TICK_RATE
 				
 				if current_time - last_anim > 1.0 / ANIM_RATE:
-					self.player.update_anims()
+					for entity in self.entities:
+						entity.update_anims(self)
 					last_anim = current_time
 
 					# Close if player dies
@@ -307,7 +355,9 @@ class Game():
 
 			self.game_map.fill((100,100,220))
 			self.game_map.blit(self.background, (0,0))
-			self.game_map.blit(self.player.texture, self.player.rect)
+
+			for entity in self.entities:
+				self.game_map.blit(entity.texture, entity.rect)
 			#game_map.blit(player.surf, player.rect)
 
 			window.fill((0,0,0))
--- res/platformer/attribution.md
+++ res/platformer/attribution.md
@@ -1,3 +1,4 @@
 
 ## Sprites:
 Animated Pixel Hero - https://rvros.itch.io/animated-pixel-hero
+Coin - https://totuslotus.itch.io/pixel-coins
