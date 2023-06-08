0b55713242919e9da13791a607445b1f3593a7e8
Daniel Ragsdale <DanJeffRags@gmail.com>
2023-06-08 12:13:56 -0600

    Allow various filtering options.

--- cli_runner_progression.py
+++ cli_runner_progression.py
@@ -20,12 +20,13 @@ URLS = [
 	"http://www.alltime-athletics.com/mmaraok.htm"
 	]
 
-event_count = len(URLS)
+event_index = 1
+
+country_filter = {} # Only count performances from the specified country, e.g. 'USA'
+ignore_losers = True # Only count the winning performance from each race. Helps elimate skew frow a single fast race in a year
+
 
 years = {key:[] for key in range(1980,2024)}
-
-event_index = 4
-
 
 print("Downloading results for " + Events[event_index])
 
@@ -34,8 +35,6 @@ soup = BeautifulSoup(response.content, "html.parser")
 page = soup.find("pre")
 
 perf_list = page.text.split("\n")
-
-country_filter = {}
 
 for t in perf_list[::-1]:
 	while t.replace("   ", "  ") != t:
@@ -58,6 +57,9 @@ for t in perf_list[::-1]:
 	if len(country_filter) and line[3] not in country_filter:
 		continue
 
+	if ignore_losers and (not str.isdigit(line[5]) or int(line[5]) > 1):
+		continue
+
 	# Parse the event time, create time_float
 	time_str = line[1]
 	if event_index < 3: # 1500,
@@ -70,6 +72,7 @@ for t in perf_list[::-1]:
 	elif event_index == 4:
 		time_list = time_str.split(":")
 		time_float = 3600 * int(time_list[0]) + 60 * int(time_list[1]) + int(re.sub("[^0-9]", "", time_list[2]))
+
 	# Parse the event year, create year
 	date_list = line[-1].split('.')
 	year = int(date_list[2])
@@ -89,51 +92,3 @@ for y in range(2000, 2024):
 		event_time = year_list[i]
 		disp_list[i] = str(datetime.timedelta(seconds=event_time))[0:7]
 	print(f"{y},{disp_list[0]},{disp_list[1]},{disp_list[2]},{disp_list[3]},{disp_list[4]},")
-
-
-#def is_dominated(candidate, elite):
-#	for i in range(0,5):
-#		if runner_prs[candidate][i] <= runner_prs[elite][i] and runner_prs[candidate][i] < 10000:
-#			return False
-#	return True
-#
-#elites = set()
-##elites.add("Kenenisa Bekele")
-#
-#print("*****************")
-#print()
-#
-#for key in runner_prs.keys():
-#	runner_dominated = False
-#
-#	# Check if it dominates any of the existing elites	
-#	# Check if any of the existing elites dominate it
-#	for elite in elites.copy():
-#		if is_dominated(key, elite):
-#			runner_dominated = True
-#		if is_dominated(elite, key):
-#			elites.remove(elite)
-#		
-#	if runner_dominated != True:
-#		elites.add(key)
-#
-#sorted_elites = list(elites)
-#sorted_elites.sort()
-#
-#for elite in sorted_elites:
-#	disp_list = [''] * 5
-#	for i in range(0, event_count):
-#		event_time = runner_prs[elite][i]
-#		if event_time < 10000:
-#			disp_list[i] = str(datetime.timedelta(seconds=event_time))[0:7]
-#		else:
-#			disp_list[i] = "       "	
-#	print(elite + " " * (40 - len(elite)) + str(disp_list))
-#
-#
-#print()
-#print("*****************")
-#
-##for key in runner_prs.keys():
-##	print(key + "  " + str(runner_prs[key]))
-#	
