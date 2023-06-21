92c21feddc9179090280af1e0ffff231cd239c4d
Daniel Ragsdale <DanJeffRags@gmail.com>
2023-06-21 13:01:16 -0600

    Displays basic chart of the results with MatPlotLib

--- cli_runner_progression.py
+++ cli_runner_progression.py
@@ -12,13 +12,13 @@ import numpy as np
 from util_sync_runners import *
 
 country_filter = {} # Only count performances from the specified country, e.g. 'USA'
-ignore_losers = True # Only count the winning performance from each race. Helps elimate skew frow a single fast race in a year
+ignore_losers = False # Only count the winning performance from each race. Helps elimate skew frow a single fast race in a year
 
 years = {key:[] for key in range(1980,2024)}
 
 # New code
 rdbc = RunnerDBConnection(RUNNER_DB_PATH)
-results = rdbc.get_event_results(EVENT_1500)
+results = rdbc.get_event_results(EVENT_5000)
 
 for r in results[::-1]:
 	if len(country_filter) and r.country not in country_filter:
@@ -36,7 +36,12 @@ for r in results[::-1]:
 
 
 # Need to manually iterate over dictionary because dictionaries are not guaranteed to preserve key order
-for y in range(2000, 2024):
+best_times = []
+fifth_times = []
+tenth_times = []
+
+output_range = range(1990, 2024)
+for y in output_range:
 	year_list = years[y]
 
 	disp_list = [''] * 5
@@ -45,7 +50,15 @@ for y in range(2000, 2024):
 		disp_list[i] = str(datetime.timedelta(seconds=event_time))[0:7]
 	print(f"{y},{disp_list[0]},{disp_list[1]},{disp_list[2]},{disp_list[3]},{disp_list[4]},")
 
+	best_times.append(year_list[0])
+	fifth_times.append(year_list[4])
+	tenth_times.append(year_list[9])
+
 fig, ax = plt.subplots()
-ax.plot([1,2,3,8], [1,4,2,3])
+
+ax.plot(output_range, best_times)
+ax.plot(output_range, fifth_times)
+ax.plot(output_range, tenth_times)
+ax.yaxis.set_major_formatter(lambda t, _: str(datetime.timedelta(seconds=t)))
 
 plt.show()
