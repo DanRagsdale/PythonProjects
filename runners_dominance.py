8622f286fa78fae3bf009955367a0feb2193e321
Daniel Ragsdale <DanJeffRags@gmail.com>
2023-07-04 10:13:34 -0600

    Reorganized and optimized code. Created basic gui boilerplate.

--- runners_dominance.py
+++ runners_dominance.py
@@ -15,69 +15,113 @@
 
 import datetime
 
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+from typing import NamedTuple
+from dataclasses import dataclass
+
 from runners_sync_data import *
 
-EVENTS = OLYMPIC_EVENTS
 
+EVENTS = OLYMPIC_EVENTS
 rdbc = RunnerDBConnection(RUNNER_DB_PATH)
 
+@dataclass
+class Runner:
+	name: str
+	bests: dict
+
+
 event_count = len(EVENTS)
-elites = set()
-runner_prs = {}
-
-for i, e in enumerate(EVENTS):
-	print("Parsing results for " + e.name)
-
-	results = rdbc.get_event_results(e)
-
-	for r in results[::-1]:
-			
-			pr_list = runner_prs.get(r.name, [100000] * event_count)
-			pr_list[i] = r.time
-
-			runner_prs[r.name] = pr_list
-
+elites = []
+runners = {}
 
 def is_dominated(candidate, elite):
-	for i in range(0,event_count):
-		if runner_prs[candidate][i] <= runner_prs[elite][i] and runner_prs[candidate][i] < 10000:
+	for e in EVENTS:
+		if e in candidate.bests and candidate.bests[e] <= elite.bests.get(e, 10000):
+		#if runner_prs[candidate][i] <= runner_prs[elite][i] and runner_prs[candidate][i] < 10000:
 			return False
 	return True
+
+for e in EVENTS:
+	print("Parsing results for " + e.name)
+
+	results = rdbc.get_event_results(FEMALE, e)
+
+	for r in results[::-1]:
+			runner = runners.get(r.name, Runner(r.name, {}))
+			runner.bests[e] = r.time
+			runners[r.name] = runner
 
 print("*****************")
 print()
 
-for key in runner_prs.keys():
+for candidate in runners.values():
 	runner_dominated = False
 
 	# Check if it dominates any of the existing elites	
 	# Check if any of the existing elites dominate it
 	for elite in elites.copy():
-		if is_dominated(key, elite):
+		if is_dominated(candidate, elite):
 			runner_dominated = True
-		if is_dominated(elite, key):
+		if is_dominated(elite, candidate):
 			elites.remove(elite)
 		
 	if runner_dominated != True:
-		elites.add(key)
+		elites.append(candidate)
 
-sorted_elites = list(elites)
-sorted_elites.sort()
 
-for elite in sorted_elites:
+elites.sort(key=lambda x: x.name)
+
+event_count = len(EVENTS)
+
+for elite in elites:
 	disp_list = [''] * event_count
-	for i in range(0, event_count):
-		event_time = runner_prs[elite][i]
+	for i, e in enumerate(EVENTS):
+		event_time = elite.bests.get(e, 10000)
 		if event_time < 10000:
 			disp_list[i] = str(datetime.timedelta(seconds=event_time))[0:7]
 		else:
 			disp_list[i] = "       "	
-	print(elite + " " * (40 - len(elite)) + str(disp_list))
+	print(elite.name + " " * (40 - len(elite.name)) + str(disp_list))
 
 
 print()
 print("*****************")
 
-#for key in runner_prs.keys():
-#	print(key + "  " + str(runner_prs[key]))
+
+class dominance_gui(tk.Frame):
+	def __init__(self, window):
+		tk.Frame.__init__(self, window)
+		
+		# GUI init	
+		window.title("The True Elite")
+		window.geometry('600x600')
+		
+		# User input	
+		sex_box = ttk.Combobox(window, state='readonly')
+		sex_box['values'] = [s.name for s in SEXES]
+		sex_box.current(0)
+		#sex_box.grid(column=0, row=0, padx=3)
+		sex_box.pack()
+
+		sex_box.bind('<<ComboboxSelected>>', lambda x: self.draw_elites(SEXES[sex_box.current()])) 
+
+		# Output Display
+		self.outputContent = StringVar()
+
+		output = ttk.Label(window)
+		output['textvariable'] = self.outputContent
+		output.pack()
+
+		window.mainloop()
+
+	def draw_elites(self, sex):
+		self.outputContent.set(sex.name)
+
+if __name__ == '__main__':
+	window = Tk()
+	gui = dominance_gui(window)
 	
